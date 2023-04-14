4c5ba6c5dbef6225428c2907d47f19d1f6b7b958
Chris McGee <chris@chrismcgee.info>
2023-04-14 14:02:47 -0400

    🦺 Fixes validation of parking in multiple zones

--- app/Http/Controllers/ParkingController.php
+++ app/Http/Controllers/ParkingController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ParkingResource;
 use App\Models\Parking;
 use App\Services\ParkingPriceService;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Validation\ValidationException;
@@ -19,18 +20,20 @@ class ParkingController extends Controller
         return ParkingResource::collection(Parking::with('vehicle', 'zone')->active()->get());
     }
 
-    public function start(Request $request): ParkingResource
+    public function start(Request $request): ParkingResource|JsonResponse
     {
         $parkingData = $request->validate([
-            'vehicle_id' => ['required', 'integer', 'exists:vehicles,id'],
+            'vehicle_id' => [
+                'required',
+                'integer',
+                'exists:vehicles,id,deleted_at,NULL,user_id,' . auth()->id(),
+            ],
             'zone_id' => ['required', 'integer', 'exists:zones,id'],
         ]);
-        $existingParking = Parking::where('vehicle_id', $request->vehicle_id)
-            ->where('zone_id', $request->zone_id)
-            ->whereNull('stop_time')
-            ->get();
-        if ($existingParking->count()) {
-            throw new ValidationException('Cannot park where you are already parked.', Response::HTTP_UNPROCESSABLE_ENTITY);
+        if (Parking::active()->where('vehicle_id', $request->vehicle_id)->exists()) {
+            return response()->json([
+                'errors' => ['general' => ['Can\'t star parking twice using same vehicle. Please stop currently active parking.']],
+            ], Response::HTTP_UNPROCESSABLE_ENTITY);
         }
 
         $parking = Parking::create($parkingData);
