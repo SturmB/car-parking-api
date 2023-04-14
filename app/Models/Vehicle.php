5ec2b763c9f3080be4ee97ff5e0c90490da8f41d
Chris McGee <chris@chrismcgee.info>
2023-04-14 14:02:18 -0400

    🗃️ Adds soft deletes to Vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Vehicle extends Model
 {
+    use SoftDeletes;
     use HasFactory;
 
     protected $fillable = ['user_id', 'plate_number', 'description'];
